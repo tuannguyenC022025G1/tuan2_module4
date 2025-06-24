d04e2c45607fef2fd54446de058e41aedb1b910e
hoangtuanc02 <tuan4695hatinh@gmail.com>
2025-06-24 08:17:36 +0700

    thuc hanh 1 -bai 7

---
+++ spring-data-customer-province-management/src/main/java/com/codegym/service/ICustomerService.java
@@ -0,0 +1,11 @@
+package com.codegym.service;
+
+import com.codegym.model.Customer;
+import com.codegym.model.Province;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+public interface ICustomerService extends IGenerateService<Customer>{
+    Iterable<Customer> findAllByProvince(Province province);
+}
